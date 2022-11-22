80a911fb3561e0e8905e18b1ac8df45b676f6f62
JHRK <jhrk05@gmail.com>
2022-11-22 10:18:27 -0300

    funny

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2022-1T-projetodev-G6
+
+# Algoritmo legal, sensacional e amante da água tônica e do pomelo.
---
+++ Script.js
@@ -0,0 +1,17 @@
+const esporrada = document.querySelector('.loading');
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.getElementById("loading").style.display = 'none';
+    }, 2000);
+
+
+})
+
+
+
+
+
+
+
+
